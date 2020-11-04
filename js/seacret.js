0534cd25128e1221edf30a2c2b7bbc4c6f4dbebf
maru_maru3 <maru.biz@gmail.com>
2020-11-04 15:21:26 +0900

    チーズアカデミーweb_課題提出

---
+++ js/seacret.js
@@ -0,0 +1,11 @@
+HTML CSS JSResult
+EDIT ON
+window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        sizeVariations: 30,
+        color: [
+            '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
+        ]
+    });
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+#CHEZE ACADEMY
+
+##プロダクトの紹介
+
+-CHEESE ACADEMYのWEBサイト
+- 学校の紹介、入学したい人向けの情報など。
+- 
